f78df3f8a8c6a3bbe12b8bd3b36c4624de5ab92b
ANDERSON2019 <anderson.sousa2017@outlook.com>
2017-03-31 12:06:32 -0300

    Add files via upload

---
+++ Robo dotnet/relatorio bit/Module1.vb
@@ -0,0 +1,121 @@
+﻿' Anderson rocha de sousa
+
+
+
+Imports System.Globalization
+Imports System.Net.Mail
+Imports System
+Imports System.Net
+Imports System.Web
+Imports System.Data.SqlClient
+Imports Excel = Microsoft.Office.Interop.Excel
+Imports relatorio_bit.clDALSQL
+
+Module Module1
+
+    Public obDal As New clDALSQL
+    Public DT As New DataTable
+    Public DT2 As New DataTable
+    Public xlApp As New Excel.Application
+    Public xlWorkBook As Excel.Workbook
+    Public xlworkSheet As Excel.Worksheet
+    Public Dbl_Linha As Double
+
+
+
+    Sub Main()
+
+        obDal.Ambiente = AmbienteExecucao.Producao
+        Buscar_dados()
+
+    End Sub
+
+    Sub Buscar_dados()
+
+        'DECLARA TODAS AS VARIAVEIS PARA UTILIZAR NO EXCEL DA PRIMEIRA PARCELA 
+
+        Dim COD_ESTIPULANTE As String = ""
+        Dim NUM_PROPOSTA As String = ""
+        Dim NUM_PARCELA As String = ""
+        Dim DAT_RETORNO_AGENTE As String = ""
+        Dim COD_AGENTE As String = ""
+        Dim COD_RETORNO As String = ""
+        Dim Diretorio As String = "C:\Users\anderson.sousa\Desktop\robos\TRABALHOS\Relatorio BIT\geradosPeloRobo\teste"
+
+
+        'BUSCAR DADOS NO BANCO - UTILIZANDO PROCEDURE SEM PARAMETROS 
+        obDal.ClearParameters()
+        DT2.Dispose()
+        DT2 = obDal.RetornaTabela("dbo.BuscaRelatorioBit", "SYSEG")
+
+
+        'ABRIR PLANILHA DE RETORNO 
+        xlWorkBook = xlApp.Workbooks.Open("C:\Users\anderson.sousa\Desktop\robos\TRABALHOS\Relatorio BIT\modelo\Arrecadação BIT.xlsx")
+
+        'SELECIONAR ABA
+        xlworkSheet = xlWorkBook.Sheets("Arrecadação BIT")
+
+        Dbl_Linha = 2
+
+        'fazer o for para prencher varial 
+
+        For Each li In DT2.Rows
+            'LIMPANDO AS VARIAVEIS
+
+            COD_ESTIPULANTE = ""
+            NUM_PROPOSTA = ""
+            NUM_PARCELA = ""
+            DAT_RETORNO_AGENTE = ""
+            COD_AGENTE = ""
+            COD_RETORNO = ""
+
+            'INSERINDO VALOR AS VARIAVEIS
+
+            COD_ESTIPULANTE = li("COD_ESTIPULANTE").ToString
+            NUM_PROPOSTA = li("NUM_PROPOSTA").ToString
+            NUM_PARCELA = li("NUM_PARCELA").ToString
+            DAT_RETORNO_AGENTE = li("DAT_RETORNO_AGENTE").ToString
+            COD_AGENTE = li("COD_AGENTE").ToString
+            COD_RETORNO = li("COD_RETORNO").ToString
+
+
+            'INSERIR OS DADoS NA PLANILHA RETORNO
+            With xlworkSheet
+
+                .Range("A" & Dbl_Linha).Value = COD_ESTIPULANTE
+                .Range("B" & Dbl_Linha).Value = NUM_PROPOSTA
+                .Range("C" & Dbl_Linha).Value = NUM_PARCELA
+                .Range("D" & Dbl_Linha).Value = DAT_RETORNO_AGENTE
+                .Range("E" & Dbl_Linha).Value = COD_AGENTE
+                .Range("F" & Dbl_Linha).Value = COD_RETORNO
+
+                'PULAR LINHA
+                Dbl_Linha = Dbl_Linha + 1
+
+            End With
+
+        Next
+
+        'SALVAR PLANILHA NO DIRETORIO 
+        xlworkSheet.SaveAs(Diretorio & Format(Now, "yyyy_MM_dd_ss") & ".xlsx")
+
+        'LIMPAR MEMORIA
+        xlworkSheet.ClearArrows()
+        xlWorkBook.Close()
+        xlApp.Quit()
+
+
+    End Sub
+
+
+End Module
+
+
+
+
+
+
+
+
+
+
---
+++ Robo dotnet/relatorio bit/clDAL.vb
@@ -0,0 +1,290 @@
+﻿Imports System.Data.SqlClient
+Imports System.Data.Common
+Imports System.Text
+
+Public Class ParameterBuilder
+    Implements IDisposable
+
+    Private _ParamName As String
+    Private _Value As Object
+    Private _DbType As DbType = Data.DbType.String
+    Private _Direction As ParameterDirection = ParameterDirection.Input
+    Private _Size As Integer = 0
+    Private _SourceColumn As String = String.Empty
+    Private _SourceVersion As DataRowVersion = DataRowVersion.Current
+    Private _SourceColumnNullMapping As Boolean = False
+
+    Public Enum TipoExecucao
+        ExecutaQuery = 0
+        RetornaParam = 1
+        RetornaTabela = 2
+    End Enum
+
+
+    Public Sub New(ByVal ParameterName As String,
+                 ByVal dbType As DbType,
+                 ByVal Value As Object,
+                 ByVal Direction As ParameterDirection,
+                 ByVal Size As Integer)
+        Me._ParamName = ParameterName
+        Me._DbType = dbType
+        Me._Value = Value
+        Me._Direction = Direction
+        Me._Size = Size
+    End Sub
+
+
+    Public Property ParamName() As String
+        Get
+            Return Me._ParamName
+        End Get
+        Set(ByVal value As String)
+            Me._ParamName = value
+        End Set
+    End Property
+
+    Public Property Value() As Object
+        Get
+            Return _Value
+        End Get
+        Set(ByVal value As Object)
+            _Value = value
+        End Set
+    End Property
+
+    Public Property DbType() As DbType
+        Get
+            Return _DbType
+        End Get
+        Set(ByVal value As DbType)
+            _DbType = value
+        End Set
+    End Property
+
+    Public Property Direction() As ParameterDirection
+        Get
+            Return _Direction
+        End Get
+        Set(ByVal value As ParameterDirection)
+            _Direction = value
+        End Set
+    End Property
+
+    Public Property Size() As Integer
+        Get
+            Return Me._Size
+        End Get
+        Set(ByVal value As Integer)
+            Me._Size = value
+        End Set
+    End Property
+
+
+#Region "IDisposable Support"
+    Private disposedValue As Boolean
+
+    Protected Overridable Sub Dispose(ByVal disposing As Boolean)
+        If Not Me.disposedValue Then
+            If disposing Then
+                _ParamName = Nothing
+                _Value = Nothing
+                _DbType = Nothing
+                _Direction = Nothing
+                _Size = Nothing
+                _SourceColumn = Nothing
+                _SourceVersion = Nothing
+                _SourceColumnNullMapping = Nothing
+            End If
+        End If
+        Me.disposedValue = True
+    End Sub
+
+    Public Sub Dispose() Implements IDisposable.Dispose
+        Dispose(True)
+        GC.SuppressFinalize(Me)
+    End Sub
+#End Region
+
+End Class
+
+Public Class clDALSQL
+    Implements IDisposable
+
+    Dim Conn As New SqlClient.SqlConnection
+    Dim Command As New SqlClient.SqlCommand
+    Dim Adapt As New SqlClient.SqlDataAdapter
+    Dim Dt As DataTable
+    'Public Parametros As New Microsoft.VisualBasic.Collection From {SqlParameter}
+    Public Parametros As New List(Of SqlParameter)
+
+    Public Enum AmbienteExecucao As Integer
+        Desenvolvimento = 0
+        Producao = 1
+        Homologacao = 2
+        DW = 3
+    End Enum
+
+    Public Enum InstanciaExecucao As Integer
+        Normal = 0
+        Alog = 1
+    End Enum
+
+
+
+    Private mAmbienteExecucao As AmbienteExecucao = AmbienteExecucao.Producao
+
+    Public Property Ambiente() As AmbienteExecucao
+        Get
+            Return mAmbienteExecucao
+        End Get
+        Set(ByVal value As AmbienteExecucao)
+            mAmbienteExecucao = value
+        End Set
+    End Property
+
+    Private Function RetornaStrConexao(ByVal NomeBanco As String, Optional ByVal Instancia As InstanciaExecucao = InstanciaExecucao.Normal) As String
+        RetornaStrConexao = ""
+        If mAmbienteExecucao = AmbienteExecucao.Producao Then
+            If Instancia = InstanciaExecucao.Normal Then
+                RetornaStrConexao = "Password=SENHA;Persist Security Info=True;User ID=lOGIN;Initial Catalog=" & NomeBanco & ";Data Source=IP"
+            ElseIf Instancia = InstanciaExecucao.Alog Then
+                RetornaStrConexao = "Password=SENHA;Persist Security Info=True;User ID=lOGIN;Initial Catalog=" & NomeBanco & ";Data Source=IP"
+            End If
+        ElseIf mAmbienteExecucao = AmbienteExecucao.DW Then
+            RetornaStrConexao = "Password=SENHA;Persist Security Info=True;User ID=lOGIN;Initial Catalog=" & NomeBanco & ";Data Source=IP"
+        Else
+            RetornaStrConexao = "Password=SENHA;Persist Security Info=True;User ID=lOGIN;Initial Catalog=" & NomeBanco & ";Data Source=IP"
+        End If
+
+    End Function
+
+    Private Function RetornaStrConexao_PostGre(ByVal NomeBanco As String) As String
+        If mAmbienteExecucao = AmbienteExecucao.Producao Then
+            RetornaStrConexao_PostGre = "Server=IP;Port=Port;UserId=integracao;Password=;Database=" & NomeBanco
+        Else
+            RetornaStrConexao_PostGre = "Server=IP;Port=Port;UserId=integracao;Password=;Database=" & NomeBanco
+        End If
+    End Function
+
+    Public Sub ClearParameters()
+        Parametros.Clear()
+    End Sub
+
+    Public Sub ExecutaProcedure(ByVal NomeProcedure As String, ByVal NomeBanco As String, Optional ByVal ExecutionTimeOut As Integer = 0, Optional ByVal Instancia As InstanciaExecucao = InstanciaExecucao.Normal)
+        Dim obParam As SqlParameter
+
+        If Conn.State = ConnectionState.Open Then
+            Conn.Close()
+        End If
+        Conn.ConnectionString = RetornaStrConexao(NomeBanco, Instancia)
+        Conn.Open()
+        Command.Connection = Conn
+        Command.CommandText = NomeProcedure
+        Command.Parameters.Clear()
+        Command.CommandType = CommandType.StoredProcedure
+        For Each obParam In Parametros
+            Command.Parameters.Add(obParam)
+        Next
+        If ExecutionTimeOut > 0 Then
+            Command.CommandTimeout = ExecutionTimeOut
+        Else
+            Command.CommandTimeout = 0
+        End If
+        Command.ExecuteNonQuery()
+
+        'APOS RETORNAR A CONSULTA ATUALIZAR OS PARAMETROS
+        Parametros.Clear()
+
+        For Each obParam In Command.Parameters
+            Parametros.Add(obParam)
+        Next
+
+        If Conn.State = ConnectionState.Open Then
+            Conn.Close()
+        End If
+
+    End Sub
+
+    Public Sub AddParameters(ByVal Nome As String, ByVal Valor As Object, ByVal Type As SqlDbType, Optional ByVal Direcao As ParameterDirection = ParameterDirection.Input, Optional ByVal Tamanho As Integer = 0)
+        Dim obParam As SqlParameter
+        obParam = New SqlParameter(Nome, Valor)
+        obParam.Direction = Direcao
+        If Tamanho > 0 Then
+            obParam.Size = Tamanho
+        ElseIf Tamanho = -1 Then
+            obParam.DbType = SqlDbType.VarChar
+            obParam.Size = -1
+        Else
+            obParam.Size = 0
+        End If
+        Parametros.Add(obParam)
+    End Sub
+
+    Public Function RetornaTabela(ByVal NomeProcedure As String, ByVal NomeBanco As String, Optional ByVal ExecutionTimeOut As Integer = 0, Optional ByVal Instancia As InstanciaExecucao = InstanciaExecucao.Normal) As DataTable
+        Dim obDT As New DataTable
+        Try
+
+            If Conn.State = ConnectionState.Open Then
+                Conn.Close()
+            End If
+            Conn.ConnectionString = RetornaStrConexao(NomeBanco, Instancia)
+            Conn.Open()
+            Command.Connection = Conn
+            Command.CommandText = NomeProcedure
+            Command.Parameters.Clear()
+            Command.CommandType = CommandType.StoredProcedure
+            For Each obParam In Parametros
+                Command.Parameters.Add(obParam)
+            Next
+            If ExecutionTimeOut > 0 Then
+                Command.CommandTimeout = ExecutionTimeOut
+            Else
+                Command.CommandTimeout = 0
+            End If
+            Adapt.SelectCommand = Command
+
+            Adapt.Fill(obDT)
+        Catch ex As Exception
+
+
+        Finally
+            RetornaTabela = obDT
+        End Try
+
+
+    End Function
+
+
+#Region "IDisposable Support"
+    Private disposedValue As Boolean ' To detect redundant calls
+
+    Protected Overridable Sub Dispose(disposing As Boolean)
+        If Not Me.disposedValue Then
+            If disposing Then
+                ' TODO: dispose managed state (managed objects).
+            End If
+            If Conn.State = ConnectionState.Open Then
+                Conn.Close()
+            End If
+            Conn.Dispose()
+            Command.Dispose()
+            Adapt.Dispose()
+            Parametros.Clear()
+            Parametros = Nothing
+            Dt = Nothing
+            ' TODO: free unmanaged resources (unmanaged objects) and override Finalize() below.
+            ' TODO: set large fields to null.
+        End If
+        Me.disposedValue = True
+    End Sub
+
+    Public Sub Dispose() Implements IDisposable.Dispose
+        Dispose(True)
+        GC.SuppressFinalize(Me)
+    End Sub
+#End Region
+
+End Class
+
+
+
